e09cd74502867d9dcb0dc5d1f4ef37b84a9347a7
sharath052005 <sharathsilangari@gmail.com>
2026-02-03 21:09:09 +0530

    Installed required Dependencies

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import { toNodeHandler } from 'better-auth/node';
+import { auth } from './lib/auth';
 
 const app = express();
 
@@ -13,8 +15,10 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+
 app.get('/', (req: Request, res: Response) => {
-    res.send('Server!');
+    res.send('Server is Live!');
 });
 
 app.listen(port, () => {
